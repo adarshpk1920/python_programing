8b6bc2117778020a321c1a5b26ae0588b826b790
jaseem-12 <121373690+jaseem-12@users.noreply.github.com>
2023-01-08 16:30:38 +0530

    Delete 9.py

--- 9.py
+++
@@ -1,8 +0,0 @@
-#Create a string from given string where first and last characters exchanged. [eg: python -
-#> nythop] 
-
-temp = "python"
-print(temp)
-a=temp[1:-1]
-b=temp[-1] + temp[1:-1] + temp[:1]
-print(b)
